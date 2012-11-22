3698aec69ceca97f653b82cf6760f451aa29cdd2
liamchzh <liamchzh@gmail.com>
2012-11-22 15:54:10 +0800

    2012.11.22

---
+++ TwoDimensional_Array.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+
+array = [0, 0, 0]
+matrix = [array*3]
+print matrix
+##[0,0,0,0,0,0,0,0,0]
+
+array = [0, 0, 0]
+matrix = []
+for i in range(3):
+    matrix.append(array)
+print matrix
+##[[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+array = [0, 0, 0]
+matrix = []
+for i in range(3):
+    matrix.append(array)
+matrix[0][1] = 1
+print matrix
+##[[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+
+array = [0, 0, 0]
+matrix = [array for i in range(3)]
+matrix[0][1] = 1
+print matrix
+##[[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+
+##直接定义
+matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+matrix[0][1] = 1
+print matrix
+##[[0, 1, 0], [0, 0, 0], [0, 0, 0]]
+
+##间接定义
+matrix = [[0 for i in range(3)] for i in range(3)]
+matrix[0][1] = 1
+print matrix
+##[[0, 1, 0], [0, 0, 0], [0, 0, 0]]
+
+##以下也是不对的
+matrix = [[0] * 3] * 3
+matrix[0][1] = 1
+print matrix
+##[[0, 1, 0], [0, 1, 0], [0, 1, 0]]
