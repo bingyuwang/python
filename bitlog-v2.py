7546916e1e0262b2c22cccea074cfd1a8c3aecf2
liamchzh <liamchzh@gmail.com>
2013-01-15 09:12:32 +0800

    finished

--- bitlog-v2.py
+++ bitlog-v2.py
@@ -1,4 +1,8 @@
 ﻿# coding:utf-8
+"""
+Author: Liam  
+E-mail: liamchzh@gmail.com
+"""
 
 import pygtk
 import gtk
@@ -15,48 +19,60 @@ class MainFrame(gtk.Window):
         super(MainFrame, self).__init__() # 初始化
 
         self.set_title("BitLog") # 标题
-        self.set_size_request(250, 100) # 窗口大小
+        self.set_size_request(250, 150) # 窗口大小
         self.set_position(gtk.WIN_POS_CENTER) # 窗口位置
         self.connect("destroy", gtk.main_quit)
+        self.set_icon_from_file("icon.png")
         
+        #LOGO "BitLog"
+        logo = gtk.Label() 
+        logo.set_use_markup(gtk.TRUE)
+        logo.set_markup('<span size="38000"><b>BitLog</b></span>')
+        
+        # 图标
         login_img = gtk.Image()
         login_img.set_from_file("login.png")
         logout_img = gtk.Image()
-        logout_img.set_from_file("login.png")
+        logout_img.set_from_file("logout.png")
         setting_img = gtk.Image()
         setting_img.set_from_file("setting.png")
         about_img = gtk.Image()
         about_img.set_from_file("about.png")
         
-        login_btn = gtk.Button() #登录按钮
+        login_btn = gtk.Button() # 登录按钮
         login_btn.set_image(login_img)
         login_btn.connect("clicked", self.login)
         login_btn.set_tooltip_text(u"登录")
+        login_btn.set_size_request(100, 40)
 
-        logout_btn = gtk.Button() #注销按钮
+        logout_btn = gtk.Button() # 注销按钮
         logout_btn.set_image(logout_img)
         logout_btn.connect("clicked", self.logout)
         logout_btn.set_tooltip_text(u"注销")
+        logout_btn.set_size_request(100, 40)
 
-        setting_btn = gtk.Button()
+        setting_btn = gtk.Button() # 设置按钮
         setting_btn.set_image(setting_img)
         setting_btn.connect("clicked", self.Se)
         setting_btn.set_tooltip_text(u"设置")
 
-        about_btn = gtk.Button()
+        about_btn = gtk.Button() # 关于按钮
         about_btn.set_image(about_img)
         about_btn.connect("clicked", self.Ab)
         about_btn.set_tooltip_text("关于")
 
+        # 控件布局
         fixed = gtk.Fixed()
-        fixed.put(setting_btn, 100,20)
-        fixed.put(about_btn, 140, 20)
-        fixed.put(login_btn, 20, 50)
-        fixed.put(logout_btn, 80, 50)
+        fixed.put(logo, 20, 20)
+        fixed.put(setting_btn, 200,20)
+        fixed.put(about_btn, 200, 50)
+        fixed.put(login_btn, 20, 85)
+        fixed.put(logout_btn, 130, 85)
 
         self.add(fixed)
         self.show_all()
-
+    
+        # 检查本地是否存有账号和密码
         global user, pswd, sta
         try:
             fuser = file('user.pkl','rb+')
@@ -73,18 +89,22 @@ class MainFrame(gtk.Window):
             pswd = ''
             sta = 0
     
+    # 登录
     def login(self, widget):
-        signal, info = log_in(user, pswd, sta)
-        if signal:
-            self.iconify()
+        if user == '' or pswd == '':
+            settings()
         else:
-            self.Message(self, info)
+            signal, info = log_in(user, pswd, sta)
+            if signal: #登录成功
+                self.iconify()
+            else: #登录失败
+                self.Message(self, info)
 
     def logout(self, widget):
         info  = log_out(user, pswd)
         self.Message(self, info)
         
-    # Settings
+    # 设置
     def Se(self, widget):
         settings()
 
@@ -119,14 +139,14 @@ class MainFrame(gtk.Window):
         md.run()
         md.destroy()
         
-    # About
+    # 关于框
     def Ab(self, widget):
         about = gtk.AboutDialog()
         about.set_program_name("BitLog")
         about.set_version("2.0")
         about.set_copyright("(c) Liam")
-        about.set_comments(u"BitLog is a ...")
-        about.set_website("http://liamchzh.com")
+        about.set_comments(u"一款用于登录外网的开源客户端")
+        about.set_website_label("http://liamchzh.com")
         about.run()
         about.destroy()
 
@@ -135,30 +155,35 @@ class settings(gtk.Window):
         super(settings, self).__init__()
         
         self.set_title("Settings")
-        self.set_default_size(250, 200)
+        self.set_default_size(250, 120)
         self.set_position(gtk.WIN_POS_CENTER)
         
-        fixed = gtk.Fixed()
-
         global user_entry, pswd_entry, sta
-        sta = 1
 
+        
+        user_label = gtk.Label(u"用户名")
+        pswd_label = gtk.Label(u"密码")
         user_entry = gtk.Entry(40)
         pswd_entry = gtk.Entry(40)
         pswd_entry.set_visibility(False)# 密码文本框字符不可见
-        user_entry.set_max_length(20)
+        user_entry.set_max_length(20) # 最大字符数
         pswd_entry.set_max_length(20)
 
         sta_btn = gtk.CheckButton(u"仅使用免费流量") # 是否国际流量
         sta_btn.connect("clicked", self.sta_change)
         
-        save_btn = gtk.Button("Save")
+        save_btn = gtk.Button(u"保存") # 保存按钮
         save_btn.connect("clicked", self.save)
+        save_btn.set_size_request(50, 30) # 按钮大小
         
-        fixed.put(user_entry, 20, 20)
-        fixed.put(pswd_entry, 20, 50)
-        fixed.put(sta_btn, 20, 80)
-        fixed.put(save_btn, 100, 80)
+        # 控件布局
+        fixed = gtk.Fixed()
+        fixed.put(user_label, 20, 25)
+        fixed.put(pswd_label, 20, 55)
+        fixed.put(user_entry, 70, 20)
+        fixed.put(pswd_entry, 70, 50)
+        fixed.put(sta_btn, 20, 85)
+        fixed.put(save_btn, 180, 80)
 
         self.add(fixed)
         self.show_all()
@@ -248,4 +273,3 @@ def log_out(user, password):
 if __name__ == '__main__':
     MainFrame()
     gtk.main()
-
