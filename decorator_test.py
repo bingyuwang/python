5c7d9e005e60f2ba5996389a77629da3d568b1af
Liam <liam@ubuntu.ubuntu-domain>
2013-02-19 12:00:22 +0800

    decorator

---
+++ decorator_test.py
@@ -0,0 +1,31 @@
+def decorator(aClass):
+    class newClass:
+        def __init__(self, age):
+            self.total_display   = 0
+            self.wrapped         = aClass(age)
+        def display(self):
+            self.total_display += 1
+            print("total display", self.total_display)
+            self.wrapped.display()
+    return newClass
+
+@decorator
+class Bird:
+    def __init__(self, age):
+        self.age = age
+    def display(self):
+        print("My age is",self.age)
+
+eagleLord = Bird(5)
+for i in range(3):
+    eagleLord.display()
+
+"""
+output:
+('total display', 1)
+('My age is', 5)
+('total display', 2)
+('My age is', 5)
+('total display', 3)
+('My age is', 5)
+"""
