f9a8f41de500e3ae1087eb7fdfeb31cf1763dfe0
liam <liamchzh@gmail.com>
2013-09-03 12:38:56 +0800

    crawl func

--- thread.py
+++ thread.py
@@ -1,57 +1,71 @@
-#-*- encoding:utf-8 -*-
-import time
+# coding:utf-8
+
+import urllib2
+import Queue
 import threading
-class mythread(threading.Thread):
-    def __init__(self, num, interval):
+
+
+urls = ["http://m.sohu.com/"]
+crawledurl = []
+
+crawl_queue = Queue.Queue()
+
+def get_next_link(page):
+    start_link = page.find("href=")
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+ 
+#¿¿¿¿
+def get_all_links(page):
+    while True:
+        url, endpos = get_next_link(page)
+        if url == None: 
+            break
+        elif url.startswith("http"):
+            #print "find:" + url
+            #urls.append(url)
+            crawl_queue.put(url)
+            page = page[endpos+1:]
+        else:
+            page = page[endpos+1:]
+
+class CrawlUrl(threading.Thread):
+    """¿¿¿¿¿¿¿html¿¿¿¿¿¿¿"""
+    def __init__(self, crawl_queue):
         threading.Thread.__init__(self)
-        self.thread_num = num
-        self.interval = interval
-        self.thread_stop = False
-
-    #ÖØĞ´run·½·¨
-    def run(self):
-        while not self.thread_stop:
-            print 'Thread Object(%d), Time:%s\n' %(self.thread_num, time.clock())
-            time.sleep(self.interval)
-
-    def stop(self):
-        self.thread_stop = True
-
-def main():
-    thread1 = mythread(1, 1)
-    thread2 = mythread(2, 2)
-    thread1.start()
-    thread2.start()
-    time.sleep(10)
-    thread1.stop()
-    thread2.stop()
-    return
-
-num = 0
-mylock = threading.RLock()
-class add_thread(threading.Thread):
-    def __init__(self, name):
-        threading.Thread.__init__(self)
-        self.t_name = name
+        self.crawl_queue = crawl_queue
 
     def run(self):
-        global num
         while True:
-            mylock.acquire()
-            print 'Thread(%s) locked, num: %d'%(self.t_name, num)
-            if num > 4:
-                mylock.release()
-                print 'thread(%s) released, num: %d' % (self.t_name, num)
-                break
-            num += 1
-            print 'Thread(%s) released, num: %d' % (self.t_name, num)
-            mylock.release()
+            url = self.crawl_queue.get()
+            if url not in crawledurl:
+                try:
+                    print "tocrawl:" + url
+                    resp = urllib2.urlopen(url)
+                    page= resp.read()
+                    get_all_links(page)
+                    crawledurl.append(url)
+                except:
+                    pass
+            else:
+                pass
+            self.crawl_queue.task_done()
 
-def test():
-    thread1 = add_thread('A')
-    thread2 = add_thread('B')
-    thread1.start()
-    thread2.start()
+    
+def main():
+    for i in range(30):
+        crawlthread = CrawlUrl(crawl_queue)
+        crawlthread.setDaemon(True)
+        crawlthread.start()
 
-if __name__ == '__main__':
-    test()
+        for url in urls:
+            crawl_queue.put(url)
+
+    crawl_queue.join()
+
+if __name__ == "__main__":
+    main()
